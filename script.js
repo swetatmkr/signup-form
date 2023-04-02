0ad44c2eef92524c3e86feba9f213ec65b42450f
swetatmkr <tamrakar5525@gmail.com>
2023-04-02 12:58:22 +0545

    signup form commited

--- script.js
+++ script.js
@@ -4,30 +4,49 @@ let errors={};
 
 submitButton.addEventListener("click",function(e){
     e.preventDefault();
+    errors ={};
     let signupForm = document.forms["signupForm"];
     let firstNameInput = signupForm.firstName;
     let lastNameInput = signupForm.lastName;
+    let emailInput = signupForm.email;
+    let passwordInput = signupForm.password;
 
-    if(firstNameInput.value=="")
+    if(firstNameInput.value== "")
     {
         errors.firstName="First name should not be empty";
     }
 
-    if(lastNameInput.value=="")
+    if(lastNameInput.value== "")
     {
         errors.lastName="Last name should not be empty";
-   
+        
+    }
+    if(emailInput.value== "")
+    {
+        errors.email="Email should not be empty";
+        
+    } 
+    if(passwordInput.value== "")
+    {
+        errors.password="Password should not be empty";
+        
+    }
+    if(passwordInput.value.length<=5)
+    {
+        errors.password="Password should be atleast 5 characters";
         
     }
 console.log(errors);
     for (let error in errors){
-
+        let lastNameGroup= document.getElementById(`${error}Group`);
+        let lastNameMessage= document.querySelector(`#${error}Group .message`);
+        lastNameGroup.classList.add("error");
+        lastNameMessage.textContent=errors[error];
 
     }
 
+    if(Object.keys(errors).length==0){
+        signupForm.submit();
+    }
 });
 
-// let lastNameGroup= document.getElementById("lastNameGroup");
-// let lastNameMessage= document.querySelector("#lastNameGroup .message");
-// lastNameGroup.classList.add("error");
-// lastNameMessage.textContent="Last name should not be empty";
